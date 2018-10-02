d7ff11566b6a14061f8b686d3f21262b03367dc4
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-10-02 14:39:04 -0300

    Add files via upload

--- PrimeiroAplicativo/FrmTabuada.cs
+++ PrimeiroAplicativo/FrmTabuada.cs
@@ -16,27 +16,23 @@ namespace PrimeiroAplicativo
         {
             InitializeComponent();
         }
-
-        private void textoVazioOuBranco()
-        {
-            if (string.IsNullOrWhiteSpace(txtNumero.Text))
-            {
-                MessageBox.Show("Você precisa digitar o número para calcular uma tabuada");
-            }
-        }
-
-
-
+        
         private void btnCalcular_Click(object sender, EventArgs e) //botão que faz o calculo
-        {
-            double numero, resultado;
-            numero = double.Parse(txtNumero.Text); //conversão de string da textbox dos números pra double.
-
-            for (int i = 0; i <=10; i++) //repete a conta dez vezes, mostrando dez vezes a tabuada do numero digitado
+        { if (string.IsNullOrWhiteSpace(txtNumero.Text))
             {
-                resultado = numero * i; //resultado da tabuada é igual á numero digitado no txtNumero * variável i.
-                txtTabuada.Text += numero + "x" + i + "=" + resultado + "\r\n"; //Faz a conta e diz pra exibir no txtTabuada a conta, 
-                //ou seja, numero x i (que vai de 0 á 10) = resultado e depois pula pra uma nova linha.
+                MessageBox.Show("Você precisa digitar o número para calcular uma tabuada", "Informação,");
+            }
+            else
+            {
+                double numero, resultado;
+                numero = double.Parse(txtNumero.Text); //conversão de string da textbox dos números pra double.
+
+                for (int i = 0; i <= 10; i++) //repete a conta dez vezes, mostrando dez vezes a tabuada do numero digitado
+                {
+                    resultado = numero * i; //resultado da tabuada é igual á numero digitado no txtNumero * variável i.
+                    txtTabuada.Text += numero + "x" + i + "=" + resultado + "\r\n"; //Faz a conta e diz pra exibir no txtTabuada a conta, 
+                    //ou seja, numero x i (que vai de 0 á 10) = resultado e depois pula pra uma nova linha.
+                }
             }
         }
 
